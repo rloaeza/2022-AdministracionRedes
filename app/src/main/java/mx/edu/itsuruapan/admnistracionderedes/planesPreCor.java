0c415255f79615d8aefcacc816161fc8613f7a9a
SergioRosas <Onlyrossnok@gmail.com>
2022-02-12 17:10:29 -0600

    Creacion del activity planesPreCor

---
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/planesPreCor.java
@@ -0,0 +1,14 @@
+package mx.edu.itsuruapan.admnistracionderedes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class planesPreCor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planes_pre_cor);
+    }
+}
