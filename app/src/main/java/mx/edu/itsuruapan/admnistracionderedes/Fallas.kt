eaa27949daf67b4a0ea3b9cf2a3f3f687c3c87bd
JosoueGPast <soyjosuep@gmail.com>
2022-02-13 15:47:14 -0600

    Creaccion de activity_fallas.xml y Fallas.kt

---
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/Fallas.kt
@@ -0,0 +1,11 @@
+package mx.edu.itsuruapan.admnistracionderedes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fallas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fallas)
+    }
+}
