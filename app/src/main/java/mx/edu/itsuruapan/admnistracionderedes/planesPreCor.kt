6ffacea7095cac6110bb20a9fa19217b2c81b9de
SergioRosas <Onlyrossnok@gmail.com>
2022-02-12 23:06:20 -0600

    actualizacion de planesPreCor de java a kotlin

--- app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/planesPreCor.kt
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/planesPreCor.kt
@@ -1,15 +1,16 @@
-package mx.edu.itsuruapan.admnistracionderedes;
+package mx.edu.itsuruapan.admnistracionderedes
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.edu.itsuruapan.admnistracionderedes.R
+import mx.edu.itsuruapan.admnistracionderedes.databinding.ActivityMainBinding
 
-import mx.edu.itsuruapan.admnistracionderedes.databinding.ActivityMainBinding;
+class planesPreCor : AppCompatActivity() {
 
-public class planesPreCor extends AppCompatActivity {
+    private lateinit var binding: ActivityMainBinding
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_planes_pre_cor);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_planes_pre_cor)
     }
 }
