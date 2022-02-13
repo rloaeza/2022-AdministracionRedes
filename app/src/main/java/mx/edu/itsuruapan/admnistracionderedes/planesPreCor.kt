f83d7b02d30e17c53f79febd40a26783ff1c5190
SergioRosas <Onlyrossnok@gmail.com>
2022-02-12 23:35:12 -0600

    cambios

--- app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/planesPreCor.kt
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/planesPreCor.kt
@@ -2,15 +2,27 @@ package mx.edu.itsuruapan.admnistracionderedes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import mx.edu.itsuruapan.admnistracionderedes.R
 import mx.edu.itsuruapan.admnistracionderedes.databinding.ActivityMainBinding
 
-class planesPreCor : AppCompatActivity() {
+class PlanesPreCor : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_planes_pre_cor)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_planes_pre_cor)
+        setContentView(binding.root)
+
+        val opciones = resources.getStringArray(R.array.planesPrevencion)
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.list_pd,
+            opciones
+        )
+
+
     }
 }
