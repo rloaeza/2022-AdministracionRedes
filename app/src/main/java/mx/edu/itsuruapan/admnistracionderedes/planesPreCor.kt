ea96a764f705783a93dcf532082f5e28ea5e5113
SergioRosas <Onlyrossnok@gmail.com>
2022-02-13 12:14:45 -0600

    cambios

--- app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/planesPreCor.kt
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/planesPreCor.kt
@@ -2,27 +2,40 @@ package mx.edu.itsuruapan.admnistracionderedes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import mx.edu.itsuruapan.admnistracionderedes.R
-import mx.edu.itsuruapan.admnistracionderedes.databinding.ActivityMainBinding
+import android.widget.Toast
+import mx.edu.itsuruapan.admnistracionderedes.databinding.ActivityPlanesPreCorBinding
 
-class PlanesPreCor : AppCompatActivity() {
+class planesPreCor : AppCompatActivity(), AdapterView.OnItemClickListener {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityPlanesPreCorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(R.layout.activity_planes_pre_cor)
+        binding = ActivityPlanesPreCorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val opciones = resources.getStringArray(R.array.planesPrevencion)
-        val adapter = ArrayAdapter(
+
+        val adapter =ArrayAdapter(
             this,
             R.layout.list_pd,
             opciones
+
         )
 
+        with(binding.autoCompleteTextView){
+            setAdapter(adapter)
+            onItemClickListener = this@planesPreCor
 
+        }
+
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, viw: View?, position: Int, id: Long) {
+      var  item = parent?.getItemAtPosition(position).toString()
+        Toast.makeText(this,item,Toast.LENGTH_SHORT)
     }
 }
