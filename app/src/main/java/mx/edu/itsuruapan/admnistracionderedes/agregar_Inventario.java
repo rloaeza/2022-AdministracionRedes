09fc4f73b330304cd646efd4b08b985363ba0276
FEC <ferchokastedw@gmail.com>
2022-02-16 02:27:05 -0600

    Buscar, editar y eliminar articulos al inventario - Funcionando

--- app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/agregar_Inventario.java
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/agregar_Inventario.java
@@ -2,6 +2,7 @@ package mx.edu.itsuruapan.admnistracionderedes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +14,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,10 +29,16 @@ import java.util.Map;
 public class agregar_Inventario extends AppCompatActivity {
 
     Button BRegistrar;
+    Button BEditar;
+    Button BEliminar;
+    Button BBuscar;
     EditText ETNombre;
     EditText ETDescripcion;
     EditText ETCantidad;
     EditText ETCategoria;
+    EditText ETid;
+
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +46,15 @@ public class agregar_Inventario extends AppCompatActivity {
         setContentView(R.layout.activity_agregar_inventario);
 
         BRegistrar=(Button)findViewById(R.id.BgRegistrar);
+        BEditar=(Button)findViewById(R.id.BgEditar);
+        BEliminar=(Button)findViewById(R.id.BgEliminar);
+        BBuscar=(Button)findViewById(R.id.BgBuscar);
+
         ETNombre=(EditText)findViewById(R.id.ETgNombre);
         ETDescripcion=(EditText)findViewById(R.id.ETgDescripcion);
         ETCantidad=(EditText)findViewById(R.id.ETgCantidad);
         ETCategoria=(EditText)findViewById(R.id.ETgCategoria);
+        ETid=(EditText)findViewById(R.id.ETgId);
 
         BRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +64,32 @@ public class agregar_Inventario extends AppCompatActivity {
                     NuevoArticulo("https://softortilla.000webhostapp.com/Servicios/insertarAlmacen.php");
                 }
                 else{ Toast.makeText(getApplicationContext(),"Rellene todos los campos",Toast.LENGTH_SHORT).show(); }
+            }
+        });
+
+        BBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ETid.length()!=0){ Buscar("https://softortilla.000webhostapp.com/Servicios/buscarAlmacen.php?IdAlmacen="+ETid.getText()+""); }
+                else{ Toast.makeText(getApplicationContext(),"Ingrese un ID",Toast.LENGTH_SHORT).show(); }
+            }
+        });
+
+        BEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ETNombre.length()!=0 & ETDescripcion.length()!=0 & ETCantidad.length()!=0 & ETCategoria.length()!=0){
+                    NuevoArticulo("https://softortilla.000webhostapp.com/Servicios/modificarAlmacen.php?IdAlmacen="+ETid.getText().toString());
+                }
+                else{ Toast.makeText(getApplicationContext(),"Rellene todos los campos",Toast.LENGTH_SHORT).show(); }
+            }
+        });
+
+        BEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ETid.length()!=0){ Eliminar("https://softortilla.000webhostapp.com/Servicios/bajaAlmacen.php?IdAlmacen="+ETid.getText().toString()); }
+                else{ Toast.makeText(getApplicationContext(),"Ingrese un ID",Toast.LENGTH_SHORT).show(); }
             }
         });
     }
@@ -77,10 +121,66 @@ public class agregar_Inventario extends AppCompatActivity {
                 return parametros;
             }
         };
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue= Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
         //.add(stringRequest);
         //stringRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS*2,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         //VolleySingleton.getIntanciaVolley(getContext()).addToRequestQueue(stringRequest);
+    }
+
+    private void Buscar(String URL){
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        ETNombre.setText(jsonObject.getString("nombre"));
+                        ETDescripcion.setText(jsonObject.getString("descripcion"));
+                        ETCantidad.setText(jsonObject.getString("cantidad"));
+                        ETCategoria.setText(jsonObject.getString("categoria"));
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"No encontrado", Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+        requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void Eliminar(String URL){
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "El producto fue eliminado", Toast.LENGTH_SHORT).show();
+                ETNombre.setText("");
+                ETCantidad.setText("");
+                ETDescripcion.setText("");
+                ETCategoria.setText("");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Algo salió mal", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            //@Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String, String>();
+                parametros.put("IdAlmacen",(ETid.getText().toString()));
+                return parametros;
+            }
+        };
+        requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
