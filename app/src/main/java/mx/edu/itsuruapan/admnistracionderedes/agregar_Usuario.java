8f5cb7dd1353b4be331f1f650eb121e347f285d4
FEC <ferchokastedw@gmail.com>
2022-02-16 18:25:33 -0600

    Menu de opciones...

--- app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/agregar_Usuario.java
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/agregar_Usuario.java
@@ -10,6 +10,7 @@ public class agregar_Usuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_usuario);
+
     }
 
 
