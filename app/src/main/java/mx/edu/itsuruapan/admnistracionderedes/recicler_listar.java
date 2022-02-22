86e603990b5e9c92d8f6a568d8b646899b069aba
yeudiel117 <fabian.yeudiel117@gmail.com>
2022-02-21 18:50:20 -0600

    reciclerView de la lista

---
+++ app/src/main/java/mx/edu/itsuruapan/admnistracionderedes/recicler_listar.java
@@ -0,0 +1,14 @@
+package mx.edu.itsuruapan.admnistracionderedes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recicler_listar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recicler_listar);
+    }
+}
